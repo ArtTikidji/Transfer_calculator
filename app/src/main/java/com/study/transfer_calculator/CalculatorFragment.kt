2909df0ddcd6ae12c0dc0fbf15e1475011a8335e
ArtTikidji <67237375+ArtTikidji@users.noreply.github.com>
2023-01-08 15:17:53 +0400

    Remove useless parts of start sample app

--- app/src/main/java/com/study/transfer_calculator/CalculatorFragment.kt
+++ app/src/main/java/com/study/transfer_calculator/CalculatorFragment.kt
@@ -8,15 +8,14 @@ import android.view.ViewGroup
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.study.transfer_calculator.databinding.FragmentFirstBinding
+import com.study.transfer_calculator.databinding.FragmentCalculatorBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CalculatorFragment : Fragment() {
     private val viewModel: MainViewModel by activityViewModels()
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCalculatorBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,8 +24,8 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -36,9 +35,6 @@ class FirstFragment : Fragment() {
 
         initTableFields()
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
         binding.buttonCalculateRatios.setOnClickListener {
             viewModel.calculateRatios()
             binding.textUsdRatio.text = "%.4f".format(viewModel.usdRatio)
